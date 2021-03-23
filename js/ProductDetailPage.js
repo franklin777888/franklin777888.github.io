a2f53b6324e300c12ac5af0aed607939e39cd2d4
Franklin Yu <franklinyu777@gmail.com>
2021-03-23 19:52:50 -0400

    Update image, product.css, Product-detail.html, ProductDetailPage.js from Doris

--- js/ProductDetailPage.js
+++ js/ProductDetailPage.js
@@ -1,6 +1,7 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);  
 const productID = urlParams.get('id');
+//function used array to insert products by id
 var product =selectProduct();
 
 function selectProduct(){
@@ -12,16 +13,23 @@ function selectProduct(){
 }
 
 window.onload=function(){
+
     if(product!=""){
+        //function to display title, price, quantity and total price
         document.querySelector('#title').innerHTML = product.productName;
         document.querySelector('#price').innerHTML = product.productPrice;
-        
+        document.getElementById("sub").setAttribute("disabled", "disabled");
+        document.getElementById("plus").addEventListener("click", plus);
+        document.getElementById("sub").addEventListener("click", subs);
+        //function to display rating stars
         var rateStars = "";
         for(let i=0;i<product.productRating;i++){
             rateStars +="<span class =\"fa fa-star checked\"></span>";
         }
         
         document.querySelector('#rating').innerHTML = rateStars;
+
+        //function to display product descriptions and images
         document.querySelector('#descriptions').innerHTML = product.productDescription;
         document.querySelector('#image1').src = product.productImage1;
         document.querySelector('#image2').src = product.productImage2;
@@ -43,4 +51,41 @@ window.onload=function(){
               imgs[i].style.display = 'block';
           }
     }
+
 }
+
+//function to display the total price
+var totalPrice = 0;
+var valueCount = 0;
+//function to calculate the total price
+function priceTotal() {
+    var unitPrice = document.querySelector('#price').innerHTML;
+    var count = document.getElementsByClassName("inputNum")[0].value;
+    totalPrice = Number(count) * unitPrice;
+    document.getElementById("tPrice").value = totalPrice.toFixed(2);
+}
+//function for plus button
+function plus() {
+    valueCount++;
+    document.getElementById("textNum").value = valueCount;
+    if (valueCount >= 1) {
+        document.getElementById("sub").removeAttribute("disabled")
+        document.getElementById("plus").classList.remove("disabled")
+    }
+    priceTotal();
+}
+// function for subs button
+function subs() {
+    valueCount--;
+    document.getElementById("textNum").value = valueCount;
+    if (valueCount == 0) {
+        document.getElementById("sub").setAttribute("disabled", "disabled")
+    }
+    priceTotal();
+}
+
+
+ 
+
+
+ 
